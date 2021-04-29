9994ddd1026ad58bcaf5bfe5d5f5c5e9def11651
Destiny Serna <destinyeserna@gmail.com>
2021-04-29 14:07:35 -0700

    Initial Commit

---
+++ Track My Expenses/Track_My_ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_My_ExpensesApp.swift
+//  Track My Expenses
+//
+//  Created by Destiny Serna on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Track_My_ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
